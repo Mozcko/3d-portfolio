d8f1faa98571aecfd59bad883bc7ac003bce9327
Mozcko <87629837+Mozcko@users.noreply.github.com>
2023-08-11 21:03:43 -0600

    about section  not complete and not working correctly idkw

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+//import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+      >
+        <div>
+          
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a relentless explorer of the digital realm. A student of computer
+        systems engineering, I thrive on dissecting intricate algorithms and
+        transforming them into tangible, user-centric solutions. My insatiable
+        curiosity fuels my journey to master the art of crafting seamless
+        software and dynamic web applications, marrying creativity with
+        precision to build the future of digital landscapes.
+      </motion.p>
 
-export default About
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
